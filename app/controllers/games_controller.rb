a865110711ca2ddd90e6a1594e654304385f1b55
Taylor Williams <taylor@thewilliams.net>
2019-03-04 23:31:08 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
-
+  before_action :set_game, only:[:show, :update]
   def create
-    @game = Game.create(state: params[:state])
+    @game = Game.create(game_params)
     render json: @game, status: 201
   end
   def show
@@ -13,5 +13,12 @@ class GamesController < ApplicationController
   end
   def index
 
+  end
+  private
+  def game_params
+    params.permit(state:[])
+  end
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
